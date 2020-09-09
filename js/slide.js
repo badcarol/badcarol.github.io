fe6f0530507d851d0efc7a70eed45910bf628201
carolchaw <carol.chaw@thegigabit.com>
2020-09-09 11:37:31 +0800

    09 update

--- js/slide.js
+++ js/slide.js
@@ -1,82 +1,79 @@
 let isDown = false;
 let startX;
 let scrollLeft;
-const dragArea = document.querySelector('.draggable');
+// const dragArea = document.querySelector('.draggable');
 const slider = document.querySelector('.items');
 const range = document.getElementById('range');
-// const item = document.getElementsByClassName('item')[2];
+const item = document.querySelectorAll(".item");
 const outerContent = document.querySelector('.wrapper');
 
 
 const end = () => {
-	isDown = false;
+    isDown = false;
     slider.classList.remove('active');
     activeClass();
-    
+
 }
 
 const start = (e) => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX || e.touches[0].pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;	
+    e.preventDefault();
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX || e.touches[0].pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
 }
 
 const move = (e) => {
-    if(!isDown) return;
+    if (!isDown) return;
     e.preventDefault();
     const x = e.pageX || e.touches[0].pageX - slider.offsetLeft;
     const dist = (x - startX);
-    slider.scrollLeft = scrollLeft - dist;  
+    slider.scrollLeft = scrollLeft - dist;
     range.value = slider.scrollLeft;
-    activeClass();  
-    
+    activeClass();
+
 }
 
-const move2 = (e) => { 
+const move2 = (e) => {
     slider.scrollLeft = range.value;
-    
+    activeClass();
 }
 
-const activeClass = () =>{
-    const li = document.querySelectorAll(".item");
+
+const activeClass = () => {
     let finalPosX;
-    for (var i = 0; i < li.length; i++) {
-        const liPosX = li[i].offsetLeft;
-        finalPosX = liPosX - slider.scrollLeft;
-        
-        if(finalPosX >= 150 && finalPosX <= 300){
-            li[i].classList.add('active');
-            li[i].classList.add('scale-up-center');
-        }else{
-            li[i].classList.remove('active');
-            li[i].classList.remove('scale-up-center');
+    for (var i = 0; i < item.length; i++) {
+        const itemPosX = item[i].offsetLeft;
+        finalPosX = itemPosX - slider.scrollLeft;
+
+        if (finalPosX >= 150 && finalPosX <= 300) {
+            item[i].classList.add('active');
+            item[i].classList.add('scale-up-center');
+        } else {
+            item[i].classList.remove('active');
+            item[i].classList.remove('scale-up-center');
         }
     }
 }
 
+const clickthrough = (e) => {
+    console.log(e)
+
+}
+
 (() => {
 
-    dragArea.addEventListener('mousedown', start);
-	dragArea.addEventListener('touchstart', start);
+    slider.addEventListener('mousedown', start);
+    slider.addEventListener('touchstart', start);
 
-	dragArea.addEventListener('mousemove', move);
-    dragArea.addEventListener('touchmove', move);
+    slider.addEventListener('mousemove', move);
+    slider.addEventListener('touchmove', move);
 
-	dragArea.addEventListener('mouseleave', end);
-	dragArea.addEventListener('mouseup', end);
-    dragArea.addEventListener('touchend', end); 
+    slider.addEventListener('mouseleave', end);
+    slider.addEventListener('mouseup', end);
+    slider.addEventListener('touchend', end);
 
-    range.addEventListener('input', move2)
-	// slider.addEventListener('mousedown', start);
-	// slider.addEventListener('touchstart', start);
+    range.addEventListener('input', move2);
 
-	// slider.addEventListener('mousemove', move);
-    // slider.addEventListener('touchmove', move);
 
-	// slider.addEventListener('mouseleave', end);
-	// slider.addEventListener('mouseup', end);
-    // slider.addEventListener('touchend', end);   
-   
-   
 })();
